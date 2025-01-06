4d49e37c72323a930e928d2887247da22d177cdf
Fathima Minha <minhasulaikar@gmail.com>
2025-01-06 20:01:01 +0530

    profile component is created

---
+++ StudentCareApp/Components/Profile.js
@@ -0,0 +1,13 @@
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react"
+
+export default function ProfilePage(){
+    const [user,setUser] = useState(null);
+    const navigation = useNavigation();
+    
+    return(
+        <>
+        
+        </>
+    )
+}
