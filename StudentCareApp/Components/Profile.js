d4b3e0a2f48e8ca43a70290478b8b160da1f98c2
Fathima Minha <minhasulaikar@gmail.com>
2025-01-06 20:17:01 +0530

    add statevariables

--- StudentCareApp/Components/Profile.js
+++ StudentCareApp/Components/Profile.js
@@ -1,10 +1,31 @@
 import { useNavigation } from "@react-navigation/native";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function ProfilePage(){
     const [user,setUser] = useState(null);
     const navigation = useNavigation();
-    
+
+    useEffect(() =>{
+        const getLoggedInUsername =async() =>{
+            try{
+                const loggedInUsername = await AsyncStorage.getItem('username');
+                const student = students.find(student => student.username === loggedInUsername);
+
+                if(student) {
+                    setUser(student);
+                } else {
+                    navigation. navigate('Login');
+                }
+            } catch(error){
+                console.error('Error fetching username from AsyncStorage',error)
+            }
+        }
+        getLoggedInUsername();
+    }, [navigation]);
+
+    if(!user){
+        return <Text> Loading... </Text>
+    }
     return(
         <>
         
