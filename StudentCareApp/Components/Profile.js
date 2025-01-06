b4a5a324110b3a6274a12ff76a9e45bb18523fcc
Fathima Minha <minhasulaikar@gmail.com>
2025-01-06 20:38:16 +0530

    add some styles-profile

--- StudentCareApp/Components/Profile.js
+++ StudentCareApp/Components/Profile.js
@@ -1,5 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import { useEffect, useState } from "react"
+import { PaperProvider } from "react-native-paper";
 
 export default function ProfilePage(){
     const [user,setUser] = useState(null);
@@ -27,8 +28,80 @@ export default function ProfilePage(){
         return <Text> Loading... </Text>
     }
     return(
-        <>
-        
-        </>
+        <PaperProvider>
+            
+        </PaperProvider>
     )
 }
+
+const styles = StyleSheet.create({
+    mainContainer:{
+        flex: 1,
+        paddingBottom : 70,
+        position: 'relative',
+    },
+    profileContent:{
+        flexGrow: 1,
+        padding :20,
+        backgroundColor: 'white',
+        borderRadius: 15,
+        overflow: 'hidden',
+    },
+    profileHeader: {
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    profilePic: {
+        width: 200,
+        height: 200,
+        marginBottom: 15,
+        boaderRadius: 75,
+        overflow: 'hidden',
+    },
+    heading: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        marginBottom: 15,
+        color: '#333',
+        textAlign: 'center',
+    },
+    email:{
+        fontSize: 16,
+        color: '#555',
+        marginBottom:10,
+    },
+    sectionTitle:{
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333',
+        marginTop:20,
+    },
+    phone:{
+        fontSize: 16,
+        color: '#555',
+        marginBottom:10,
+        textAlign:'left',
+    },
+    address:{
+        fontSize: 16,
+        color: '#555',
+        textAlign: 'left',
+    },
+    leftAligned: {
+        textAlign :'left',
+        width: '100%',
+        paddingLeft: 10,
+    },
+    seperator:{
+        width: '80%',
+        height:1,
+        backgroundColor: '#ddd',
+        marginVertical: 10,
+        alignSelf: 'center',
+    },
+    footerMenu: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+    }
+})
