1d49975787688a53d2e148c9ad4a98c5fe826595
Fathima Minha <minhasulaikar@gmail.com>
2025-01-06 22:46:34 +0530

    updated the course file

--- StudentCareApp/Components/Course.js
+++ StudentCareApp/Components/Course.js
@@ -1,53 +1,56 @@
-import { useNavigation } from "@react-navigation/native"
-import { useEffect, useState } from "react";
-import { students } from "../assets/StudentsDb";
-import { courses } from "../assets/StudentsDb";
-import { PaperProvider } from "react-native-paper";
-import { View,StyleSheet,Text,Button } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { PaperProvider } from 'react-native-paper';
+import { students } from '../assets/StudentsDb'; 
+import { courses } from '../assets/StudentsDb'; 
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import FooterMenu from '../common/FooterMenu'; 
+import Footer from '../common/Footer';
 
-
-export default function Course(){
+export default function Course() {
     const navigation = useNavigation();
     const [studentData, setStudentData] = useState(null);
     const [courseData, setCourseData] = useState(null);
 
-    useEffect(()=>{
-        const getStudentData = async() =>{
-            try{
+    useEffect(() => {
+        const getStudentData = async () => {
+            try {
                 const username = await AsyncStorage.getItem('username');
 
-                if(username){
-                    const student = students.find(student=> student.username === username);
-                    if(username){
+                if (username) {
+                    const student = students.find(student => student.username === username);
+                    if (student) {
                         setStudentData(student);
                         const course = courses.find(course => course.id === student.course_id);
                         setCourseData(course);
                     }
                 }
-            } catch(error){
+            } catch (error) {
                 console.error('Error fetching username from AsyncStorage', error);
             }
         };
-        getStudentData();
-    },[]) ;
 
-    if(!studentData || !courseData){
-        return(
+        getStudentData();
+    }, []);
+
+    if (!studentData || !courseData) {
+        return (
             <View style={styles.loadingContainer}>
-                <Text> Loading... </Text>
+                <Text>Loading...</Text>
             </View>
-        )
+        );
     }
-    return(
+
+    return (
         <PaperProvider>
             <View style={styles.container}>
-                <Text style ={styles.courseName}>{courseData.name}</Text>
-                <Text style={styles.courseCode}>Code: {courseData.course_code} | Dept: {courseData.department} </Text>
+                <Text style={styles.courseName}>{courseData.name}</Text>
+                <Text style={styles.courseCode}>Code: {courseData.course_code} | Dept: {courseData.department}</Text>
 
-                <View style = {styles.separator} />
+                <View style={styles.separator} />
 
-                <Text style={[styles.sectionTitle, styles.leftAligned]}> Course Information </Text>
+                <Text style={[styles.sectionTitle, styles.leftAligned]}>Course Information</Text>
                 <Text style={[styles.courseInfo, styles.leftAligned]}>Code: {courseData.course_code}</Text>
                 <Text style={[styles.courseInfo, styles.leftAligned]}>Department: {courseData.department}</Text>
                 <Text style={[styles.courseInfo, styles.leftAligned]}>Duration: {courseData.duration}</Text>
@@ -55,8 +58,11 @@ export default function Course(){
                 
                 <View style={styles.separator} />
             </View>
+
+            <Footer />
+            <FooterMenu />
         </PaperProvider>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
